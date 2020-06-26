920859230e8bd4defec5c60b33783c18b103325c
Raymond Gao <30156758@qq.com>
2020-06-26 23:22:55 +0800

    Initial commit for python test project 01

---
+++ PythonTestStep01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# My First Python Program
+'''
+First Program including:
+1.Hello World!
+2.Data Type
+'''
+print('---------------------------------')
+print("Hello World Python!")
+
+print('---------------------------------')
+str1='StringTest'
+str2='''StringTest
+2'''
+
+print(str1)
+print(str1[0:-1])
+print(str1[0:5])
+print(str1[3:6])
+print(str1*2)
+print(str1 + ' Completed')
+print(str2)
+print('Hello\nWorld!')
+print(r'Hello\nWorld!')
+
